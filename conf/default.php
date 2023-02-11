e00731e6af5e4d3d3f1281871ad7b3cd14864179
Roques Steve <steve.roques@gmail.com>
2023-02-11 05:06:25 +0100

    init plugin

---
+++ conf/default.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Iframeinterwiki plugin, default configuration settings
+ *
+ * @author    Steve Roques <steve.roques@gmail.com>
+ */
+$conf['js_ok'] = false;
---
+++ conf/metadata.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Iframeinterwiki plugin, configuration metadata
+ *
+ * @author    Steve Roques <steve.roques@gmail.com>
+ */
+$meta['js_ok']  = array('onoff','_caution' => 'security');
